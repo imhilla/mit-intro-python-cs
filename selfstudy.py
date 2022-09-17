060c58d97467fd5f6a49958d74c8a02b510c3a61
imhilla <hillaryodhiambo282@gmail.com>
2022-09-17 15:54:53 +0300

    add lecture 2

---
+++ selfstudy.py
@@ -0,0 +1,15 @@
+print("Yankees rule!")
+print("But not in boston!")
+print("yankees rule", "But bot in boston")
+print(type(3))
+print(type(3.0))
+print(type("hello world"))
+print(type(True))
+print(6//2)  # weird syntax huh!
+print(6/2)  # returns a float value
+
+# multiple assignments
+x, y = 67, 48
+x, y = y, x
+print("x = ", x)
+print("y = ", y)
